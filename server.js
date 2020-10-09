0c3ee6e16376333c5cfe9119314b92ac0339cec7
Matheus Miliorini <miliorinimatheus@gmail.com>
2020-10-09 14:29:37 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var prerender = require('./lib');
 
 var server = prerender({
-  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ]
+  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
+  chromeLocation: '/usr/bin/google-chrome'
 });
 
 server.use(prerender.sendPrerenderHeader());
